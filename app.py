4e3de10099d1aa4a2b65a85c48572e9bdf64e593
oppositecategory <69627013+oppositecategory@users.noreply.github.com>
2021-11-08 14:47:06 +0200

    Updated dashboard. TODO: Add form to insert parameters of ARIMA model.

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
-import matplotlib.pyplot as plt  
 import numpy as np 
 import pandas as pd 
-import yfinance as yf 
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 
+import matplotlib 
+import matplotlib.pyplot as plt  
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+from statsmodels.tsa.seasonal import seasonal_decompose 
+from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
+
+import seaborn as sns 
+import yfinance as yf 
+
+import os
 import io 
 import base64
 from model import GridSearchARIMA, convert_timeseries_stationary
-from joblib import load
+
+matplotlib.use('Agg')  # turn off gui
+sns.set(rc={'figure.figsize':(8,3)}) # Use seaborn backend 
 
 def get_stock_data(stock_name):
     return yf.download(stock_name,
                        start='2015-01-01',
-                       end='2021-01-01') 
+                       end='2021-11-07') 
 
 def requestResults(stock_name):
     df = get_stock_data(stock_name)
@@ -22,20 +31,13 @@ def requestResults(stock_name):
     d, ts = stat_data.values()
     model = GridSearchARIMA(ts,d)
     curr_time = ts.index.values[-1]
-    predictions = model.predict(curr_time,curr_time + np.timedelta64(30*4,'D'))
+    predictions = model.predict(curr_time,curr_time + np.timedelta64(30*10,'D'))
     return pd.DataFrame(predictions)
 
-
-def _create_plot(data,title):
+def extract_plot_from_fig(fig):
     """ Create a matplotlib plot without the need to locally save it.
         Source: https://gitlab.com/-/snippets/1924163
     """ 
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    axis.set_title(title)
-    axis.grid()
-    axis.plot(data)
-
     # Convert plot to PNG image
     pngImage = io.BytesIO()
     FigureCanvas(fig).print_png(pngImage)
@@ -46,7 +48,10 @@ def _create_plot(data,title):
     return pngImageB64String
 
 
+PLOTS_FOLDER = os.path.join('static','plots')
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = PLOTS_FOLDER
  
 @app.route('/')
 def home():
@@ -58,12 +63,52 @@ def get_data():
         user = request.form['search']
         return redirect(url_for('success',name=user))
 
-@app.route('/success/<name>')
+@app.route('/stock/<name>')
 def success(name):
-    stock = get_stock_data(name)
-    img1 = _create_plot(stock['Close'],'Stock price')
+    df = get_stock_data(name)
 
-    return render_template("analysis.html", name=name, data=requestResults(name).to_html(), image=img1) 
+    volume_fig,ax = plt.subplots()
+    sns.lineplot(x=df.index,y=df['Volume'],label='Volume',ax=ax)
+    sns.lineplot(x=df.index,y=df['Volume'].rolling(window=12).mean(),label='Averaged volume',ax=ax)
+    ax.set_title('Volume of stock over time')
+    volume_img = extract_plot_from_fig(volume_fig)
+    
+
+    fig, (ax1,ax2,ax3) = plt.subplots(3,figsize=(8,5))
+    df.groupby(df.index.day).mean().plot(y=['High','Close','Low'],ax=ax1,xlabel='Day');
+    df.groupby(df.index.month).mean().plot(y=['High','Close','Low'],ax=ax2, xlabel='Month');
+    df.groupby(df.index.year).mean().plot(y=['High','Close','Low'], ax=ax3, xlabel='Year');
+    plt.tight_layout()
+    stock_img = extract_plot_from_fig(fig)
+
+    decompose_fig = seasonal_decompose(df['Close'],period=365,model='additive').plot();
+    decompose_fig.set_size_inches(10,5)
+    decompose_img = extract_plot_from_fig(decompose_fig)
+
+    acf_fig = plot_acf(df['Close'].dropna());
+    pacf_fig = plot_pacf(df['Close'].diff(2).dropna());
+
+    acf_img = extract_plot_from_fig(acf_fig);
+    pacf_img = extract_plot_from_fig(pacf_fig);
+
+    predictions = requestResults(name)
+    
+    pred_fig = plt.figure()
+    df['Close'].plot(fig=pred_fig);
+    predictions['predicted_mean'].plot(fig=pred_fig);
+    pred_img = extract_plot_from_fig(pred_fig)
+
+
+
+    return render_template("analysis.html", 
+                            name=name, 
+                            data=pd.DataFrame(df.describe()['Close']).to_html(), 
+                            image=volume_img,
+                            image1=stock_img,
+                            image2=decompose_img,
+                            image3=acf_img,
+                            image4=pacf_img,
+                            image5=pred_img)
 
 
 if __name__ == '__main__':
